f57c65f01dbd564499ef3619447072c8fd8221d4
yenohp <devinanderson286@gmail.com>
2022-04-12 13:47:24 -0700

    Add new button -- dedicate reset button to clear TODO: Grid range set grid size after clearing Add color swatch Add dedicated random color button     Add new button to switch back to color swatch

--- README.md
+++ README.md
@@ -1,1 +1,8 @@
 # etch-a-sketch
+Add more buttons:
+Dedicated grid input
+Dedicated color swatch
+Dedicated color randomizer
+
+Color swatch appears by default:
+when random button is clicked, color swatch is removed & new button is added to re-enable swatch
--- script.js
+++ script.js
@@ -1,6 +1,12 @@
 const body = document.querySelector('body');
-const resetBttn = document.querySelector('button');
-let divCount = 16; //default amount
+const resetBttn = document.querySelector('.reset');
+
+const randomBttn = document.querySelector('.random');
+const gridSelector = document.querySelector('#gridSize');
+const sizeContainer = document.querySelector('.sizeContainer');
+const h4 = document.querySelector('h4');
+let gridSize = gridSelector.value;
+let divCount = gridSize; //default amount
 
 const container = document.querySelector('.container');
 let outers = document.querySelectorAll('.outer');
@@ -29,6 +35,8 @@ function makeGrid(divCount) {
 }
 
 function deleteGrid(outers) {
+    // See if there are any nodes
+    if (!container.childElementCount) return;
     for (let outer of outers) {
         container.removeChild(outer);
 
@@ -54,13 +62,17 @@ makeGrid(divCount);
 
 // Reset color and change number of squares
 resetBttn.addEventListener('click', () => {
-    for (let inner of inners) {
-        inner.style.backgroundColor = 'white';
-    }
-    divCount = parseInt(prompt('How many squares?'));
-    if (divCount > 30) divCount = 30;
+
+    if (divCount > 64) divCount = 64;
     else if (divCount < 0) divCount = 1;
     deleteGrid(outers);
     makeGrid(divCount);
 
+})
+
+// Show change in values in real time
+// Change divs in real time
+gridSelector.addEventListener('change', (e) => {
+    let gridSize = e.target.value;
+    h4.innerText = `${gridSize} x ${gridSize}`;
 })
